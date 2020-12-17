ff4626aa1f386d4f77b3dc6deb05dd86f6cd3fd2
Alessandro Stagni <alessandro@redshield.co>
2020-12-17 15:33:43 +1300

    Added react + typescript folder structure for frontend. Tested with tutorial app, working fine.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import * as ReactDOM from 'react-dom';
+import {Game} from './components/Game'
+
+function App() : any{
+  return ReactDOM.render(<Game />, document.getElementById("root"));
+}
+
+export default App;
