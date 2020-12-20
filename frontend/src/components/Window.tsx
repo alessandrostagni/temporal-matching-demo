b1c71339a546e5c878315e04655ac8441230816f
Alessandro Stagni <alessandro@redshield.co>
2020-12-21 12:06:23 +1300

    Implemented small flask server for querying for a matching given an input formula, gamma and assignment.

--- frontend/src/components/Window.tsx
+++ frontend/src/components/Window.tsx
@@ -22,11 +22,30 @@ export class Window extends Component<WindowProps, WindowState>{
       this.setState({graphData: 'Clicked!'})
   }
 
+  request_graph() {
+    fetch('https://mywebsite.com/endpoint/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstParam: 'yourValue',
+        secondParam: 'yourOtherValue',
+      })
+  }
+
   render() {
     return (
       [
         <ClauseInput submitHandler={() => this.handleRequest()}/>,
         <Graph graphData = {graphData}></Graph>
+        {userData.map((data,id)=>{
+          return <div key={id}>
+            <h2>{data.first_name} {data.last_name}</h2>
+            <p>{data.email}</p>
+          </div>
+        })}
       ]
     );
   }
