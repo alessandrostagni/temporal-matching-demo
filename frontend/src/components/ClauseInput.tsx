c52a032d5429618b2eee1de9ea8e3faa6e0ccab2
Alessandro Stagni <alessandro@redshield.co>
2020-12-21 15:31:27 +1300

    Working on frontend.

--- frontend/src/components/ClauseInput.tsx
+++ frontend/src/components/ClauseInput.tsx
@@ -1,23 +1,46 @@
 import React, {Component} from "react";
+import { Button, TextField } from '@material-ui/core';
+
 
 interface ClauseInputProps {
-  submitHandler: any;
 }
 
 interface ClauseInputState {
-  value : string;
+  formula: string;
+  assignment: JSON
+  gamma: number;
 }
 
 export class ClauseInput extends Component<ClauseInputProps, ClauseInputState> {
   constructor(props: any) {
     super(props);
     this.state = {
-      value: 'Please write an essay about your favorite DOM element.'
+      formula: '',
+      assignment: JSON.parse('{}'),
+      gamma: -1
     };
 
   }
 
+  handleRequest() {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(this.state)
+    };
+    fetch('https://localhost:3000/get-graph', requestOptions)
+        .then(response => console.log(response.json()))
+  }
+
   render() {
-    return <button onClick={this.props.submitHandler} />
+    return ([
+      <TextField style={{display: 'inline'}} onChange={(e) => {this.setState({formula: e.target.value})}}/>,
+      <br/>,
+      <TextField style={{display: 'inline'}} onChange={(e) => {this.setState({assignment: JSON.parse(e.target.value)})}}/>,
+      <br/>,
+      <TextField style={{display: 'inline'}} onChange={(e) => {this.setState({gamma: Number(e.target.value)})}}/>,
+      <br/>,
+      <Button variant="contained" color="primary" onClick={() => this.handleRequest()}>Compute!</Button>
+    ]);
   }
 }
--- frontend/src/components/Window.tsx
+++ frontend/src/components/Window.tsx
@@ -1,9 +1,9 @@
 import React, {Component} from "react";
 import { ClauseInput } from "./ClauseInput";
+// import { Graph } from "./Graph";
 
 
-interface WindowProps {
-    
+interface WindowProps {   
 }
 
 interface WindowState{
@@ -15,37 +15,12 @@ export class Window extends Component<WindowProps, WindowState>{
   constructor(props: any) {
       super(props);
       this.state = {graphData: ''}
-      //this.handleRequest = this.handleRequest.bind(this);
-  }
-
-  handleRequest() {
-      this.setState({graphData: 'Clicked!'})
-  }
-
-  request_graph() {
-    fetch('https://mywebsite.com/endpoint/', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        firstParam: 'yourValue',
-        secondParam: 'yourOtherValue',
-      })
   }
 
   render() {
     return (
       [
-        <ClauseInput submitHandler={() => this.handleRequest()}/>,
-        <Graph graphData = {graphData}></Graph>
-        {userData.map((data,id)=>{
-          return <div key={id}>
-            <h2>{data.first_name} {data.last_name}</h2>
-            <p>{data.email}</p>
-          </div>
-        })}
+        <ClauseInput />
       ]
     );
   }
